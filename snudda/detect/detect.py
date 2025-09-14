8d15e1f1d176480a0325d8d8d8eb9cc30ef80f0a
wstho <scottwilliamthompson@gmail.com>
2025-09-14 23:41:49 +0200

    detect change

--- snudda/detect/detect.py
+++ snudda/detect/detect.py
@@ -753,7 +753,7 @@ class SnuddaDetect(object):
             hyper_voxel_id = list({k for k, v in self.hyper_voxels.items() for t in targets if t in v['neurons'] and 'dend' in v['neurons'][t]})
 
             # hyper_voxel_id = np.unique(np.concatenate([rng.choice(hyper_voxel_id, size = min(round(np.random.normal(3, 0.5)), len(hyper_voxel_id)), replace = False), rng.choice(self.get_hypervoxel_coords_and_section_id(neuron = neuron)['neuron'][:,0], size =2, replace = False)]))
-            hyper_voxel_id = rng.choice(hyper_voxel_id, size = min(round(np.random.normal(5, 0.5)), len(hyper_voxel_id)), replace = False)
+            hyper_voxel_id = rng.choice(hyper_voxel_id, size = min(round(np.random.normal(6, 0.5)), len(hyper_voxel_id)), replace = False)
 
             return hyper_voxel_id
 
@@ -1763,7 +1763,7 @@ class SnuddaDetect(object):
         xyz = vox_idx*self.voxel_size + self.hyper_voxel_origo
         inside_idx = np.where(np.sum(np.bitwise_and(0 <= vox_idx, vox_idx < self.hyper_voxel_size), axis=1) == 3)[0]
 
-        n_syn_cap = 100*len(set(target_ids))            
+        n_syn_cap = 200*len(set(target_ids))            
         rand_idx = np.random.permutation(len(inside_idx))[:n_syn_cap]
         inside_idx = inside_idx[rand_idx]
         
