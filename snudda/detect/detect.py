d7c89ba05b47bc998c2a3468b321c52c2f83e4ec
wstho <scottwilliamthompson@gmail.com>
2025-09-20 18:56:52 +0200

    wfqe

--- snudda/detect/detect.py
+++ snudda/detect/detect.py
@@ -740,7 +740,7 @@ class SnuddaDetect(object):
         elif neuron.axon_density_type == "new_sparse":
 
             rng = np.random.default_rng(seed)
-            hyper_voxel_id = list({k for k, v in self.hyper_voxels.items() for t in neuron.axon_targets if t in v['neurons'] and 'dend' in v['neurons'][t]})
+            hyper_voxel_id = list({k for k, v in self.hyper_voxels.items() for t in neuron.axon_targets if t in v['neurons'] and 'soma' in v['neurons'][t]})
             # dend_field = self.get_hypervoxel_coords_and_section_id(neuron = neuron)['neuron'][:,0]
             # hyper_voxel_id = np.unique(np.concatenate([rng.choice(hyper_voxel_id, size = min(5, len(hyper_voxel_id)), replace = False), rng.choice(dend_field, size = min(5, len(dend_field)), replace = False)]))
             # hyper_voxel_id = list({k for k, v in self.hyper_voxels.items() if v['neurons']})
@@ -1749,20 +1749,20 @@ class SnuddaDetect(object):
         
         # targets = targets[targets != 0]
         # print(targets)
-        n_vox_cap = 100
+        # n_vox_cap = 100
         mask_3d = np.any(np.isin(self.dend_voxels, targets), axis=3)
         vox_idx = np.column_stack(np.where(mask_3d))
         # print(len(vox_idx))
-        soma_dists = self.dend_soma_dist[vox_idx[:, 0], vox_idx[:, 1], vox_idx[:, 2], :]
-        distance_mask = np.logical_and(soma_dists > -1, soma_dists <= prox_dist).any(axis=1)
-        vox_idx = vox_idx[distance_mask]
+        # soma_dists = self.dend_soma_dist[vox_idx[:, 0], vox_idx[:, 1], vox_idx[:, 2], :]
+        # distance_mask = np.logical_and(soma_dists > -1, soma_dists <= prox_dist).any(axis=1)
+        # vox_idx = vox_idx[distance_mask]
         # print(len(vox_idx))
         xyz = vox_idx*self.voxel_size + self.hyper_voxel_origo
         
-        rand_idx = np.random.permutation(len(xyz))[:n_vox_cap]
+        # rand_idx = np.random.permutation(len(xyz))[:n_vox_cap]
         
-        return xyz[rand_idx, :], vox_idx[rand_idx, :]
-        # return xyz, vox_idx
+        # return xyz[rand_idx, :], vox_idx[rand_idx, :]
+        return xyz, vox_idx
 
     
     
